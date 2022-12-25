77e7511f58d4e42267f62973a1161c0e787b9722
gloginov <georgeloginov90@gmail.com>
2022-12-25 13:37:53 +0500

    init app *backend mariadb, expressjs, adminjs *front react *front nuxt3

---
+++ .docker/backendjs/start.sh
@@ -0,0 +1,13 @@
+npm install
+#npm run dev
+#npm run dev
+
+#set -e
+#
+#if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
+#  set -- node "$@"
+#fi
+#
+#exec "$@"
+
+#CMD ["node"]
---
+++ .docker/node-nuxt/start.sh
@@ -0,0 +1,12 @@
+npm install
+npm run dev
+
+#set -e
+#
+#if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
+#  set -- node "$@"
+#fi
+#
+#exec "$@"
+
+#CMD ["node"]
